8c01644859cf420a776b171c55d7441adf0928ae
Natalia Dushkina <smirnovanatalia2008@gmail.com>
2018-11-01 13:13:54 +0300

    Updates

---
+++ precache-manifest.079c700c84c072f8703ef234dfa2c253.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "17eccf555681633b61e1",
+    "url": "/properties/static/js/runtime~main.17eccf55.js"
+  },
+  {
+    "revision": "abfcbe99a6509d0b1cdf",
+    "url": "/properties/static/js/main.abfcbe99.chunk.js"
+  },
+  {
+    "revision": "c349ab5edd000561db14",
+    "url": "/properties/static/js/1.c349ab5e.chunk.js"
+  },
+  {
+    "revision": "abfcbe99a6509d0b1cdf",
+    "url": "/properties/static/css/main.4aca61e5.chunk.css"
+  },
+  {
+    "revision": "5a86cdf901ed6154b79832ec2c9b1d63",
+    "url": "/properties/index.html"
+  }
+];
