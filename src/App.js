7d28569ee47e668e349b3d6798df8a0af5ef524f
Natalia Dushkina <smirnovanatalia2008@gmail.com>
2018-11-01 09:28:34 +0300

    initial app structure

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './pages/About/About';
+import Home from './pages/Home/Home';
+import './App.css';
+
+const MainMenu = () => (
+  <div>
+    <Link to="/">
+      <button>Главная</button>
+    </Link>
+    <Link to="/about">
+      <button>О нас</button>
+    </Link>
+  </div>
+)
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <div>Ongrad</div>
+            <MainMenu />
+          </header>
+          <div>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
