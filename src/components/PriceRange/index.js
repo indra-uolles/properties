0cb60203e2698d00af34b1ca898a14a44870da9d
Natalia Dushkina <smirnovanatalia2008@gmail.com>
2018-11-03 15:06:01 +0300

    fix price range

--- src/components/PriceRange/index.js
+++ src/components/PriceRange/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Range } from 'rc-slider';
 import 'rc-slider/assets/index.css';
+import './style.css';
 
 export default class PriceRange extends React.Component {
   state = {
@@ -11,8 +12,12 @@ export default class PriceRange extends React.Component {
 
   render() {
     const { min, max, minStart, maxStart } = this.state;
+    const marks = {
+      [minStart]: minStart.toLocaleString('ru-RU'),
+      [maxStart]: maxStart.toLocaleString('ru-RU')
+    };
     return (
-      <React.Fragment>
+      <div className='price-range'>
         <div className='properties-form__price'>{min.toLocaleString('ru-RU')} — {max.toLocaleString('ru-RU')}</div>
         <Range 
           min={minStart} 
@@ -24,8 +29,9 @@ export default class PriceRange extends React.Component {
               max: e[1]
             }, () => this.props.onChange(this.state));
           }}
+          marks={marks}
         />
-      </React.Fragment>
+      </div>
     );
   }
 }
